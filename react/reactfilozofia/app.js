c6c16dc3064ad52c732cad779902c5c29cbea01a
Nagy Martin Kristóf 206 <nagmar206@hengersor.hu>
2024-12-09 12:24:29 +0100

    React

---
+++ react/reactfilozofia/app.js
@@ -0,0 +1,29 @@
+//natív (hogyan?)
+
+const btn = document.createElement('button');
+
+btn.onclick = function()
+{
+    alert("Ez egy natív kód!");
+}
+
+btn.innerHTML = "Natív Gomb";
+
+document.getElementById("nativ-button-container").appendChild(btn);
+
+//react (mit akarok látni?)
+
+const gomb = React.createElement("button", 
+{
+    onclick: function ()
+    {
+        alert("Ez egy react kód");
+    },
+}, 
+    "React gomb"
+)
+
+//első paraméter az elemet amit hasznáéni szeretnénk
+//második paraméter egy referencia egy natív DOM elemre, hivatkozásipont megadása
+ReactDOM.render(gomb,document.getElementById("react-button-container"));
+
